8bddac9359f545b4142380cc54f2dcdaf83bbc3b
leogr <leo.gru.as@outlook.com>
2022-10-19 20:51:40 -0300

    Adicionar arquivos de projeto.

---
+++ Parking/Pages/Clients/Model.cshtml.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Parking.Pages.Clients
+{
+    public class ModelModel : PageModel
+    {
+        public List<ClientInfo> listClient = new List<ClientInfo>();
+        public void OnGet()
+        {
+            try
+            {
+                //conexão com o BD
+                String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=Crud/parking;Integrated Security=True";
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    if(conn.State != ConnectionState.Open) { Console.WriteLine("Conectado"); } else { Console.WriteLine("Não conectado"); }
+                    // comando sql
+                    var sql = "SELECT * FROM clients";
+                    //comando para permitir a leitura do comando sql
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.Id = "" + reader.GetInt32(0);
+                                clientInfo.Name = reader.GetString(1);
+                                clientInfo.Email = reader.GetString(2);
+                                clientInfo.Phone = reader.GetString(3);
+                                clientInfo.Car = reader.GetString(4);
+                                clientInfo.placa = reader.GetString(5);
+                                clientInfo.cor = reader.GetString(6);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+
+                                listClient.Add(clientInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception:" + ex.ToString());
+            }
+          
+        }
+    }
+    public class ClientInfo
+    {
+        public string Id;
+        public string Name;
+        public string Email;
+        public string Phone;
+        public string Car;
+        public string placa;
+        public string cor;
+        public string created_at;
+    }
+}
